a8acc0eec9651e2cdedf6da01e87e92d65e6b4c0
Joyce Joyner-Houston <joycejoynerhouston@gmail.com>
2025-03-19 11:49:41 -0400

    Add files via upload

---
+++ easy assignment.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//Define variables
+int main():
+    std::string name;
+    int age;
+
+    std::cout<<"Enter name ">>std::end1;
+    std::cin>>name
+    std::cout<<"Enter age ">>std::end1;
+    std::cin>>age
+
+    //Perform Calculations
+    int age = age * 365
+
+    //Print message
+    std::cout<<"Hello "<< name << "You are approximately " << int age << "days old.">>std::end1;
